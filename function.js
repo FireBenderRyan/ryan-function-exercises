643dd7f66625eca8d0675c74bc38d601faf8f4b2
FireBenderRyan <cooltardis995@gmail.com>
2018-12-16 19:44:40 -0500

    Copleted one started 2 and 3

--- function.js
+++ function.js
@@ -1,15 +1,21 @@
 //your code here
 //1. Create a function called threeMinusTwo that returns the difference of 3 - 2
-
+var threeMinusTwo = function(){
+	var signal = '3-2'
+	return signal
+}
 
 //2. Create a function called returnString that returns a string
-
-
+var returnString = function(){
+	return 'string'
+}
 
 //3. Create a function called returnVar that returns the returnThisVar variable
 var returnThisVar = 'Var returned!';
-
-
+var rerurnVar = function(){
+	return returnThisVar;
+}
+ 
 //4. Create a function called addVars that adds the values of valueOne and valueTwo
 var valueOne = 5;
 var valueTwo = 5; 
